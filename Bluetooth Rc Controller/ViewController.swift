f922bb5c5b951a6764eed0fa230de34a8c066da6
Onder Guler <onder.glr@hotmail.com>
2024-08-11 15:26:59 +0300

    Initial Commit

---
+++ Bluetooth Rc Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bluetooth Rc Controller
+//
+//  Created by Onder Guler on 11.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
